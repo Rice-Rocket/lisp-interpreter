a726e9eeaa234170a6c2bf6025d32e66a8a328dc
Killian Hourihan <kllnhrhn@gmail.com>
2024-09-16 12:01:00 -0700

    Added user input

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
 
 int main() {
-    printf("Hello, world!");
+    puts("Lisp Version 0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    while (1) {
+        char *input = readline("Lisp > ");
+        add_history(input);
+
+        printf("%s\n", input);
+
+        free(input);
+    }
 
     return 0;
 }
